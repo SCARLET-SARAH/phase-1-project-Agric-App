ea36fcaced9f7bfb067d54fc007ddb5ee23b06a5
scarlet <sarahscarlet641@gmail.com>
2024-04-13 09:39:43 +0300

    complete

--- server.js
+++
@@ -1,8 +0,0 @@
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/', router)
-server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server is running')
-})
